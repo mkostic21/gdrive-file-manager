adaffb192dacd0c92311d717e5932ad89364f39e
mimi <mislav.kostic@gmail.com>
2019-03-24 22:34:50 +0100

    Finally created it.

---
+++ File Manager/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.Upload;
+
+namespace DriveFileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(System.Environment.CurrentDirectory);
+            Console.ReadKey(); //any key terminates the program
+        }
+    }
+}
