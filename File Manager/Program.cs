cc73d96a0b83cf0a7c6a285d3760e21214ec3329
mimi <mislav.kostic@gmail.com>
2019-03-28 00:38:49 +0100

    First upload EVER successful.

--- File Manager/Program.cs
+++ File Manager/Program.cs
@@ -1,24 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
-using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive;
 using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
-using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Util.Store;
-using Google.Apis.Upload;
+using Google.Apis.Services;
 
 namespace DriveFileManager
 {
     class Program
     {
+        static string[] Scopes = { DriveService.Scope.Drive, DriveService.Scope.DriveFile };
+
         static void Main(string[] args)
         {
-            Console.WriteLine(System.Environment.CurrentDirectory);
-            Console.ReadKey(); //any key terminates the program
+            try
+            {
+                new Program().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("ERROR: " + e.Message);
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+
+            UserCredential credential;
+            using (var stream = new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+            {
+
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, Scopes, "user", CancellationToken.None);
+            }
+
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GDrive File Manager",
+            });
+
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = "test.jpg"
+            };
+            FilesResource.CreateMediaUpload request;
+            using (var stream = new FileStream("test.jpg",
+                                    FileMode.Open))
+            {
+                request = service.Files.Create(
+                    fileMetadata, stream, "image/jpeg");
+                request.Fields = "id";
+                request.Upload();
+            }
+            var file = request.ResponseBody;
+            Console.WriteLine("File ID: " + file.Id);
         }
     }
 }
